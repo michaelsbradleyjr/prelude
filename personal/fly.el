eb0ac81330257b49cf38ade5a89805e5660f07e2
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2021-12-03 20:21:55 -0600

    [personal] fly.el

---
+++ personal/fly.el
@@ -0,0 +1,3 @@
+(set-face-attribute 'flycheck-info nil :foreground "yellow" :background "blue")
+(set-face-attribute 'flycheck-warning nil :foreground "blue" :background "yellow")
+(set-face-attribute 'flycheck-error nil :foreground "yellow" :background "red")
