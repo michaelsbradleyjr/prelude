70262f6a3c5e02816b6ad874f788cb6de32f0ccd
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-02-08 11:13:42 -0600

    [personal] rev typescript.el

--- personal/typescript.el
+++ personal/typescript.el
@@ -2,3 +2,10 @@
 	     :ensure t
 	     :hook
 	     (typescript-mode . lsp))
+
+(defun prettier ()
+  "Run prettier on file save via pnpm script."
+  (when (eq major-mode 'typescript-mode)
+    (shell-command-to-string (format "prettier --plugin-search-dir . --write . %s" buffer-file-name))))
+
+(add-hook 'after-save-hook #'prettier)
