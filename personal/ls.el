1065df90b6e594e961fcc6eb3ae33ccc6630e317
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-04-20 12:22:25 -0500

    [personal] ls.el

---
+++ personal/ls.el
@@ -0,0 +1,4 @@
+(when (string= system-type "darwin")
+  (setq dired-use-ls-dired t
+        insert-directory-program "/usr/local/bin/gls"
+        dired-listing-switches "-aBhl --group-directories-first"))
